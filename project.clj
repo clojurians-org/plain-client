078500f266c1fd888d6767df1112c5f657986e0d
clojurians-org <larluo@clojurians.org>
2017-06-25 13:08:21 +0800

    add protobuf

--- project.clj
+++ project.clj
@@ -3,13 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-thriftc "0.2.3"]]
+  :plugins [[lein-thriftc "0.2.3"]
+            [lein-protobuf "0.4.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.thrift/libthrift "0.10.0"]]
-  
+                 [org.apache.thrift/libthrift "0.10.0"]
+                 [com.google.protobuf/protobuf-java "2.5.0"]]
   :hooks [leiningen.thriftc]
   :thriftc {:path "thrift"
             :source-paths ["src/thrift"]
             :target-path "src"}
+  :proto-path "src/proto"
   :java-source-paths ["src/java" "src/gen-java"]
   :source-paths ["src/clojure"])
